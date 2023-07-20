bd3a3b34809546b82b7c7ef47eef43dedf886607
Sam Shariat <samshariat7@gmail.com>
2023-07-20 18:00:47 +0330

    improving seo

--- components/Layout/Seo.tsx
+++ components/Layout/Seo.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { SITE_DESCRIPTION, SITE_TITLE, SITE_URL, SOCIAL_TWITTER, TWITTER_URL } from 'core/utils/constants'
+import { SITE_DESCRIPTION, SITE_FULL_DESCRIPTION, SITE_TITLE, SITE_URL, SOCIAL_TWITTER, TWITTER_URL } from 'core/utils/constants'
 import { DefaultSeo } from 'next-seo'
 
-export function Seo() {
+export default function Seo() {
   const origin = SITE_URL;
   return (
     <DefaultSeo
-      title={SITE_TITLE}
       defaultTitle={SITE_TITLE}
-      titleTemplate={`%s | ${SITE_TITLE}`}
-      description={SITE_DESCRIPTION}
+      titleTemplate={`%s | ${SITE_DESCRIPTION}`}
+      description={SITE_FULL_DESCRIPTION}
+      canonical={origin}
+      themeColor={'#101212'}
       defaultOpenGraphImageWidth={1200}
       defaultOpenGraphImageHeight={550}
       openGraph={{
@@ -18,16 +19,31 @@ export function Seo() {
         url: origin,
         images: [
           {
-            url: `${origin}/origin.png`,
+            url: `${origin}/vidorigin.png`,
             alt: `${SITE_TITLE} Open Graph Image`,
           },
         ],
       }}
       twitter={{
         handle: `@${SOCIAL_TWITTER}`,
-        site: `@${TWITTER_URL +SOCIAL_TWITTER}`,
+        site: `@${SOCIAL_TWITTER}`,
         cardType: 'summary_large_image',
       }}
+      additionalLinkTags={[
+        {
+          rel: 'icon',
+          href: `/logos/vidicon.svg`,
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: `/logos/vidicon.png`,
+          sizes: '76x76'
+        },
+        {
+          rel: 'manifest',
+          href: '/manifest.json'
+        },
+      ]}
     />
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import ManageSection from 'components/sections/ManageSection';
+import Seo from 'components/Layout/Seo'
 import { SITE_DESCRIPTION, SITE_FULL_DESCRIPTION, SITE_TITLE } from 'core/utils/constants';
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>{SITE_TITLE} | {SITE_DESCRIPTION}</title>
+        <title>{`${SITE_TITLE} | ${SITE_DESCRIPTION}`}</title>
         <meta name="description" content={SITE_FULL_DESCRIPTION} />
         <link rel="icon" type="image/svg+xml" href="/logos/vidicon.svg"/>
         <link rel="icon" type="image/png" href="/logos/vidicon.png"/>
       </Head>
-
+      <Seo />
       {/* <ClaimSection /> */}
       <ManageSection />
     </>
